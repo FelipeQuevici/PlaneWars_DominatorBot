aab8d51a854c29218ab6a5cf61d93255efa1e668
Joe Rossi <joethered@gmail.com>
2016-02-02 13:52:55 -0800

    Coordinated attack strategy.  Once we have the largest fleet, the strategy will iterate through each of the enemy planets and find their nearest ally.  If we have planets between the target planet and their ally (so their reinforcements can't make it to them on time), we will initiate a coordinated attack.  The attack iterates through each of our planets within range and excludes planets currently being attacked. It then assigns each remaining planet how many forces they are responsible for sending based on their relative population size in the coalition.  For improvements, I think that a better check than us having the largest fleet could be used to initiate this strategy.  Also I think the calculation of the number of our forces each planet should send could be improved.

--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -27,19 +27,21 @@ def setup_behavior_tree():
     # Top-down construction of behavior tree
     root = Selector(name='High Level Ordering of Strategies')
 
-    defensive_plan = Sequence(name="Defensive Plan")
+    '''defensive_plan = Sequence(name="Defensive Plan")
     defend_planets = Action(defend_planet)
-    defensive_plan.child_nodes = [defend_planets]
+    defensive_plan.child_nodes = [defend_planets]'''
 
     '''offensive_plan = Sequence(name='Offensive Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_weakest_enemy_planet)
     offensive_plan.child_nodes = [largest_fleet_check, attack]'''
-
+    
     offensive_plan = Sequence(name='Offensive Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_enemy)
-    offensive_plan.child_nodes = [largest_fleet_check, attack]
+    coordianted_attack = Action(coordinate_attack_on_enemy)
+    enemy_exist_check = Check(enemy_has_multiple_planets)
+    offensive_plan.child_nodes = [largest_fleet_check, coordianted_attack] #[largest_fleet_check, attack]
 
     spread_sequence = Sequence(name='Spread Strategy')
     neutral_planet_check = Check(if_neutral_planet_available)
@@ -51,7 +53,7 @@ def setup_behavior_tree():
     spread_action = Action(spread_to_weakest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]'''
 
-    root.child_nodes = [defensive_plan,offensive_plan,spread_sequence]
+    root.child_nodes = [offensive_plan,spread_sequence, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
